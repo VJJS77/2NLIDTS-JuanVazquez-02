27128bf919ccd8c31cddd297bb707212b2637960
Juan Vázquez <juvas4325@gmail.com>
2026-08-16 22:57:38 -0600

    Codigo 1 del profesor.

--- 2NLIDTS-JuanVazquez-02.cpp
+++ 2NLIDTS-JuanVazquez-02.cpp
@@ -1,19 +1,31 @@
-// 2NLIDTS-JuanVazquez-02.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+int casa = 0;
+using namespace std;
+//funcion secundaria
+void funcion_secundario(int numero) {
+	cout << "El número recibido es: " << endl;
+	cout << numero << endl;
+}
+int funcion_secundario_dos(int numero_dos) {
 
+	int resultado = 0;
+	cout << "el número recibido es: " << endl;
+	cout << numero_dos << endl;
+	resultado = numero_dos * numero_dos;
+	return resultado;
+}
+//funcion principal
 int main()
 {
-    std::cout << "Hello World!\n";
+	int valor_recibido;
+	int variable = 0;
+	cout << "Actividad 02 - programa estructurado básico" << endl;
+	cout << "ingrese el valor a imprimir" << endl;
+	cin >> variable;
+	funcion_secundario(variable);
+	valor_recibido = funcion_secundario_dos(variable);
+	cout << "el resultado de la multiplicación es: " << endl;
+	cout << valor_recibido << endl;
+	return 0;
+
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
